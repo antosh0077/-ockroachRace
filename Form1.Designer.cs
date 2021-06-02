2608a24e04b707408cac9255597e0f7e87be7390
Andranik Khachatryan <53318183+antosh0077@users.noreply.github.com>
2021-06-02 15:15:43 +0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace СockroachRace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.winnerLbl = new System.Windows.Forms.Label();
+            this.cockroach3PB = new System.Windows.Forms.PictureBox();
+            this.cockroach1PB = new System.Windows.Forms.PictureBox();
+            this.cockroach2PB = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cockroach3PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cockroach1PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cockroach2PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(12, 823);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 3;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // winnerLbl
+            // 
+            this.winnerLbl.AutoSize = true;
+            this.winnerLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 60.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.winnerLbl.ForeColor = System.Drawing.Color.Red;
+            this.winnerLbl.Location = new System.Drawing.Point(226, 250);
+            this.winnerLbl.Name = "winnerLbl";
+            this.winnerLbl.Size = new System.Drawing.Size(0, 92);
+            this.winnerLbl.TabIndex = 10;
+            // 
+            // cockroach3PB
+            // 
+            this.cockroach3PB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(195)))), ((int)(((byte)(195)))));
+            
+            this.cockroach3PB.Image = global::СockroachRace.Properties.Resources._100x83_cockoch_;
+            this.cockroach3PB.Location = new System.Drawing.Point(87, 571);
+            this.cockroach3PB.Name = "cockroach3PB";
+            this.cockroach3PB.Size = new System.Drawing.Size(115, 73);
+            this.cockroach3PB.TabIndex = 9;
+            this.cockroach3PB.TabStop = false;
+            // 
+            // cockroach1PB
+            // 
+            this.cockroach1PB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(195)))), ((int)(((byte)(195)))));
+            this.cockroach1PB.Image = global::СockroachRace.Properties.Resources._100x83_cockoch_;
+            this.cockroach1PB.Location = new System.Drawing.Point(87, 165);
+            this.cockroach1PB.Name = "cockroach1PB";
+            this.cockroach1PB.Size = new System.Drawing.Size(115, 73);
+            this.cockroach1PB.TabIndex = 8;
+            this.cockroach1PB.TabStop = false;
+            // 
+            // cockroach2PB
+            // 
+            this.cockroach2PB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(195)))), ((int)(((byte)(195)))));
+            this.cockroach2PB.Image = global::СockroachRace.Properties.Resources._100x83_cockoch_;
+            this.cockroach2PB.Location = new System.Drawing.Point(87, 369);
+            this.cockroach2PB.Name = "cockroach2PB";
+            this.cockroach2PB.Size = new System.Drawing.Size(115, 73);
+            this.cockroach2PB.TabIndex = 7;
+            this.cockroach2PB.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::СockroachRace.Properties.Resources.Race___;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1601, 796);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1626, 861);
+            this.Controls.Add(this.winnerLbl);
+            this.Controls.Add(this.cockroach3PB);
+            this.Controls.Add(this.cockroach1PB);
+            this.Controls.Add(this.cockroach2PB);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cockroach3PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cockroach1PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cockroach2PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox cockroach2PB;
+        private System.Windows.Forms.PictureBox cockroach1PB;
+        private System.Windows.Forms.PictureBox cockroach3PB;
+        private System.Windows.Forms.Label winnerLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace СockroachRace
+{
+    public partial class Form1 : Form
+    {
+        //ThreadManager threadManager;
+        Random rnd;
+        public Form1()
+        {
+            InitializeComponent();            
+            rnd = new Random();
+        }
+
+        bool someCockroachFinished = false;
+
+        public void CockroachRun(object cockroach)
+        {
+            //if (someCockroachFinished)
+            //    Thread.CurrentThread.Abort();
+
+            PictureBox pb = (PictureBox)cockroach;
+            while (pb.Location.X < 1410 && !someCockroachFinished)
+            {                
+                Thread.Sleep(rnd.Next(0, 20));
+                Action action = () => pb.Location = new Point(pb.Location.X + rnd.Next(0, 20), pb.Location.Y);// + rnd.Next(0, 10)- rnd.Next(0, 10));  
+                Invoke(action);
+            }
+            
+            
+            if (!someCockroachFinished)
+            {
+                string winnerName = Thread.CurrentThread.Name;
+                Action action = () => winnerLbl.Text = $"The winner is {winnerName}!!!";
+                Invoke(action);
+                someCockroachFinished = true;
+                Thread.CurrentThread.Abort();
+            }
+            else
+                Thread.CurrentThread.Abort();
+
+        }
+
+        private void GetReady()
+        {
+            string winnerName = string.Empty;
+            for (int i = 3; i > 0; i--)
+            {
+                Action action = () => winnerLbl.Text = $"{i}";
+                Invoke(action);
+                Thread.Sleep(1000);
+            }
+
+            Action action1 = () => winnerLbl.Text = string.Empty;
+            Invoke(action1);
+
+            Thread t1 = new Thread(new ParameterizedThreadStart(CockroachRun));
+            t1.Name = "Cockroach №1";
+            //t1.Priority = ThreadPriority.Lowest;
+            t1.Start(cockroach1PB);
+
+            Thread t2 = new Thread(new ParameterizedThreadStart(CockroachRun));
+            t2.Name = "Cockroach №2";
+            //t2.Priority = ThreadPriority.Lowest;
+            t2.Start(cockroach2PB);
+
+            Thread t3 = new Thread(new ParameterizedThreadStart(CockroachRun));
+            t3.Name = "Cockroach №3";
+            //t3.Priority = ThreadPriority.Lowest;
+            t3.Start(cockroach3PB);
+           
+            Thread.CurrentThread.Abort();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            Thread t0 = new Thread(new ThreadStart(GetReady));
+            //t0.Priority = ThreadPriority.Highest;
+            t0.Start();
+
+            cockroach1PB.Location = new Point(87, 165);
+            cockroach2PB.Location = new Point(87, 369);
+            cockroach3PB.Location = new Point(87, 571);
+            someCockroachFinished = false;
+            winnerLbl.Text = string.Empty;
+        }
+    }
+}
